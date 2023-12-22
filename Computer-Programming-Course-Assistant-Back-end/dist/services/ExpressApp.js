1dad9cdeb31b2fe58d7736910caab089c485e3de
Bamlaku <bamlakhiruya@gmail.com>
2023-12-22 22:04:07 +0300

    build configuration

--- Computer-Programming-Course-Assistant-Back-end/dist/services/ExpressApp.js
+++ Computer-Programming-Course-Assistant-Back-end/dist/services/ExpressApp.js
@@ -12,14 +12,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const body_parser_1 = __importDefault(require("body-parser"));
+const express_1 = __importDefault(require("express"));
 const routes_1 = require("../routes");
+const cors_1 = __importDefault(require("cors"));
 const App = (app) => __awaiter(void 0, void 0, void 0, function* () {
     app.get('/', (req, res) => {
         res.send('hello express');
     });
-    app.use(body_parser_1.default.json());
-    app.use(body_parser_1.default.urlencoded({ extended: true }));
+    app.use((0, cors_1.default)());
+    app.use(express_1.default.json());
+    app.use(express_1.default.urlencoded({ extended: true }));
     app.use('/api/v1/auth', routes_1.AuthRoutes);
     app.use('/api/v1/student', routes_1.StudentRoutes);
     return app;
